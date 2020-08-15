2a93b5c40e13591cf5a0edbb09446c5ed4a5ced4
dqn <suyarizzz@gmail.com>
2020-08-16 04:43:53 +0900

    feat: add tokenizer

--- src/tokenize.ts
+++ src/tokenize.ts
@@ -1,4 +1,82 @@
-export function tokenize(src: string) {
-  console.log('hello');
-  return;
+export type Token =
+  | {
+      kind: 'eof';
+    }
+  | {
+      kind: 'reserved';
+      str: string;
+    }
+  | {
+      kind: 'print';
+    }
+  | {
+      kind: 'ident';
+      str: string;
+    }
+  | {
+      kind: 'num';
+      val: number;
+    };
+
+export function tokenize(src: string): Token[] {
+  const tokens: Token[] = [];
+  let cur = src;
+
+  const next = (num: number): string => {
+    return cur.slice(0, num);
+  };
+
+  const strshift = (num: number): string => {
+    const s = cur.slice(0, num);
+    cur = cur.slice(num);
+    return s;
+  };
+
+  const consume = (str: string): boolean => {
+    if (next(str.length) === str && !/\w/.test(cur[str.length])) {
+      strshift(str.length);
+      return true;
+    }
+
+    return false;
+  };
+
+  while (cur) {
+    if (/\s/.test(next(1))) {
+      strshift(1);
+      continue;
+    }
+
+    if (['+', '-', '*', '/', '(', ')', ';', '='].includes(next(1))) {
+      tokens.push({ kind: 'reserved', str: strshift(1) });
+      continue;
+    }
+
+    if (consume('print')) {
+      tokens.push({ kind: 'print' });
+      continue;
+    }
+
+    if (/[a-z_]/.test(next(1))) {
+      let str = '';
+
+      while (/\w/.test(next(1))) {
+        str += strshift(1);
+      }
+
+      tokens.push({ kind: 'ident', str });
+      continue;
+    }
+
+    const val = parseInt(cur);
+    if (!isNaN(val)) {
+      strshift(val.toString().length);
+      tokens.push({ kind: 'num', val });
+      continue;
+    }
+
+    throw new Error(`unknown token ${cur.slice(0, 10)}...`);
+  }
+
+  return tokens;
 }
--- test/parser.test.ts
+++ test/parser.test.ts
@@ -1,5 +1,42 @@
-describe('describe', () => {
-  test('test', () => {
-    expect(1).toBe(1);
+import { Token, tokenize } from '../src/tokenize';
+
+describe('tokenize', () => {
+  test('tokenize', () => {
+    const src = `
+foo = 1;
+bar = 2;
+print(1 + 2 * (3 + 4) - foo * bar);
+`;
+
+    const expected: Token[] = [
+      { kind: 'ident', str: 'foo' },
+      { kind: 'reserved', str: '=' },
+      { kind: 'num', val: 1 },
+      { kind: 'reserved', str: ';' },
+      { kind: 'ident', str: 'bar' },
+      { kind: 'reserved', str: '=' },
+      { kind: 'num', val: 2 },
+      { kind: 'reserved', str: ';' },
+      { kind: 'print' },
+      { kind: 'reserved', str: '(' },
+      { kind: 'num', val: 1 },
+      { kind: 'reserved', str: '+' },
+      { kind: 'num', val: 2 },
+      { kind: 'reserved', str: '*' },
+      { kind: 'reserved', str: '(' },
+      { kind: 'num', val: 3 },
+      { kind: 'reserved', str: '+' },
+      { kind: 'num', val: 4 },
+      { kind: 'reserved', str: ')' },
+      { kind: 'reserved', str: '-' },
+      { kind: 'ident', str: 'foo' },
+      { kind: 'reserved', str: '*' },
+      { kind: 'ident', str: 'bar' },
+      { kind: 'reserved', str: ')' },
+      { kind: 'reserved', str: ';' },
+    ];
+
+    const tokens = tokenize(src);
+    expect(tokens).toEqual(expected);
   });
 });
