253eb615c4f4457b74bfd212225ffcf4c0362046
dqn <suyarizzz@gmail.com>
2020-08-23 03:15:04 +0900

    feat: implement compile

---
+++ src/bigbrain.ts
@@ -0,0 +1,10 @@
+import { generateCode } from './code';
+import { parse } from './parse';
+import { tokenize } from './tokenize';
+
+export function compile(src: string): string {
+  const tokens = tokenize(src);
+  const nodes = parse(tokens);
+  const code = generateCode(nodes);
+  return code;
+}
